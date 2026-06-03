b05c5a3336c4ded243329849bb18259f4b5d81c2
samuedo435 <samuedo435@gmail.com>
2026-06-03 11:15:59 -0500

    Estructura inicial veltix en spring boot

---
+++ src/main/java/com/veltrix/VeltrixApplication.java
@@ -0,0 +1,13 @@
+package com.veltrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeltrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeltrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/veltrix/VeltrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veltrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeltrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8244ce400eba3e2f1353d9da02b4ee22a1bcccd9
samuedo435 <samuedo435@gmail.com>
2026-06-03 23:15:42 -0500

    Se agregan métodos CRUD para categoria en Controller y Sevice

--- src/main/java/com/veltrix/controller/CategoriaController.java
+++ src/main/java/com/veltrix/controller/CategoriaController.java
@@ -21,8 +21,26 @@ public class CategoriaController {
         return categoriaService.listarTodas();
     }
 
+    @GetMapping("/{id}")
+    public Categoria obtenerCategoria(@PathVariable Long id) {
+        return categoriaService.obtenerPorId(id);
+    }
+
     @PostMapping
     public Categoria guardarCategoria(@RequestBody Categoria categoria) {
         return categoriaService.guardar(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public Categoria actualizarCategoria(
+            @PathVariable Long id,
+            @RequestBody Categoria categoria) {
+
+        return categoriaService.actualizar(id, categoria);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarCategoria(@PathVariable Long id) {
+        categoriaService.eliminar(id);
     }
 }
--- src/main/java/com/veltrix/service/CategoriaService.java
+++ src/main/java/com/veltrix/service/CategoriaService.java
@@ -31,4 +31,26 @@ public class CategoriaService {
     public List<Categoria> listarTodas() {
         return categoriaRepository.findAll();
     }
+    
+    public Categoria obtenerPorId(Long id) {
+        return categoriaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+    }
+
+    public Categoria actualizar(Long id, Categoria categoriaActualizada) {
+
+        Categoria categoria = obtenerPorId(id);
+
+        categoria.setNombre(categoriaActualizada.getNombre());
+        categoria.setDescripcion(categoriaActualizada.getDescripcion());
+
+        return categoriaRepository.save(categoria);
+    }
+
+    public void eliminar(Long id) {
+
+        Categoria categoria = obtenerPorId(id);
+
+        categoriaRepository.delete(categoria);
+    }
 }

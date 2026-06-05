2c123ca7f9cc27612e52ea0181f0a0b791cf0eb7
samuedo435 <samuedo435@gmail.com>
2026-06-04 19:39:47 -0500

    Se crea el filtro de autenticacion jwt

--- src/main/java/com/veltrix/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/veltrix/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,69 @@
 package com.veltrix.security.jwt;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtAuthenticationFilter
+        extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService) {
+
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader =
+                request.getHeader("Authorization");
+
+        if (authHeader == null
+                || !authHeader.startsWith("Bearer ")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token =
+                authHeader.substring(7);
+
+        if (jwtService.validarToken(token)) {
+
+            String correo =
+                    jwtService.extraerCorreo(token);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            correo,
+                            null,
+                            Collections.emptyList());
+
+            authentication.setDetails(
+                    new WebAuthenticationDetailsSource()
+                            .buildDetails(request));
+
+            SecurityContextHolder.getContext()
+                    .setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
